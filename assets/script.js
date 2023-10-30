cabe35852758a9668194f24f8373c26c0a448d9a
Amanda Norris <amandakathleennorris@gmail.com>
2023-10-30 15:12:46 -0500

    code to get searched location then 5 day forecast for it

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,33 @@
 var apiKey = "59603c63fa51fa7eaa74959c5bdaf3f9";
 
-function checkAPI () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=" + apiKey)
-    .then(function (response) {
-        if (response.ok) {
-            console.log(response.json());
-        }
-    })
+function getWeather() {
+    //getting the search box's value
+    var citySearched = document.getElementById("citysearch").value;
+
+    //checking to be sure the box has a value
+    if (citySearched) {
+        //using openweather's geocoding API to get the latitude and longitude
+        fetch("https://api.openweathermap.org/geo/1.0/direct?q=" +citySearched + "&limit=1&appid=" + apiKey)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then (function (data){
+                    //storing lat and lon values
+                    var cityLat = data[0].lat;
+                    var cityLon = data[0].lon;
+
+                    //getting 5 day forecast for the selected city
+                    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" +cityLon + "&appid=" + apiKey)
+                    .then(function (response) {
+                        if (response.ok) {
+                            response.json().then (function (data) {
+                                console.log(data);
+                            })
+                        }
+                    })
+                })
+            }
+        })
+    }
 }
 
-checkAPI();
+document.getElementById("search-btn").addEventListener("click", getWeather);
